51ef34aefbc4268743c331337a0f0bb0ced1793a
Sean Kelley <seansfkelley@gmail.com>
2014-06-25 23:29:17 -0700

    paging works with empty view

--- ios-drinks/PagingRecipeViewController.swift
+++ ios-drinks/PagingRecipeViewController.swift
@@ -9,11 +9,23 @@
 import Foundation
 import UIKit
 
+extension UIViewController: Equatable {}
+
+// This is probably not the smartest way to do this. Will this leak out and cause weird issues elsewhere
+// since I'm extending a builtin class?
+func == (lhs: UIViewController, rhs: UIViewController) -> Bool {
+    return lhs === rhs
+}
+
 class PagingRecipeViewController: UIPageViewController, UIPageViewControllerDataSource {
     var allRecipeResults: RecipeSearchResult[] = []
     var currentResultIndex: Int = -1
 
+    var _allViewControllers: UIViewController[] = []
+
     override func viewDidLoad() {
+        super.viewDidLoad()
+
         var detailsController = self.storyboard.instantiateViewControllerWithIdentifier("recipeDetailViewController") as RecipeDetailViewController
         var blankViewController = self.storyboard.instantiateViewControllerWithIdentifier("blankViewController") as UIViewController
         detailsController.allRecipeResults = self.allRecipeResults
@@ -21,24 +33,34 @@ class PagingRecipeViewController: UIPageViewController, UIPageViewControllerData
 
         self.dataSource = self
 
+        self._allViewControllers = [ detailsController, blankViewController ]
+
         self.setViewControllers([ detailsController ], direction: .Forward, animated: true, completion: nil)
     }
 
     func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
-        println(viewController)
+        if let i = find(self._allViewControllers, viewController) {
+            if i + 1 < self._allViewControllers.count {
+                return self._allViewControllers[i + 1]
+            }
+        }
         return nil
     }
 
     func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
-        println(viewController)
+        if let i = find(self._allViewControllers, viewController) {
+            if i - 1 >= 0 {
+                return self._allViewControllers[i - 1]
+            }
+        }
         return nil
     }
 
-    func presentationCountForViewController(pageViewController: UIPageViewController) -> Int {
-        return 3
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return self._allViewControllers.count
     }
 
     func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
-        return 1
+        return 0
     }
 }
